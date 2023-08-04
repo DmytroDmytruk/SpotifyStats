852afb48de049997bed7feab3bdfdebfdec989fb
DmytroDmytruk <dimadmytruk2015@gmail.com>
2023-08-04 22:41:27 +0300

    can get code

--- SpotifyStats/src/main/java/com/example/demo/Controller/StatController.java
+++ SpotifyStats/src/main/java/com/example/demo/Controller/StatController.java
@@ -1,36 +1,43 @@
 package com.example.demo.Controller;
 
+import java.net.URI;
+import java.net.URL;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/spotify")
 public class StatController {
 
     private static final String CLIENT_ID = "7ea1798ef6a540319a8413ce0c0c012e";
-    private static final String REDIRECT_URI = "callback";
+    private static final String REDIRECT_URI = "http://localhost:8080/spotify/callback";
 
     @GetMapping("/login")
-    public String login() {
+    public ResponseEntity<Void> login(HttpServletResponse httpServletResponse) {
         String state = generateRandomString(16);
-        String scope = "user-read-private user-read-email";
+        String scope = "user-read-private%20user-read-email";
         String redirectURL = "https://accounts.spotify.com/authorize?" +
                 "response_type=code&" +
                 "client_id=" + CLIENT_ID + "&" +
                 "scope=" + scope + "&" +
                 "redirect_uri=" + REDIRECT_URI + "&" +
                 "state=" + state;      
-        RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject(redirectURL, String.class);
         int i = 5;
-        return response;
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(redirectURL)).build();
+       
     }
       
     @GetMapping("/callback")
-    public void callback() {
-    	
+    public String callback(@RequestParam(value = "code") String code) {
+    	return code;
     }
     
     private String generateRandomString(int length) {
