d599c794b8c1794e428ec1ab890eaf42782a4622
DmytroDmytruk <dimadmytruk2015@gmail.com>
2023-08-04 23:21:16 +0300

    ...

--- SpotifyStats/src/main/java/com/example/demo/Controller/StatController.java
+++ SpotifyStats/src/main/java/com/example/demo/Controller/StatController.java
@@ -2,9 +2,14 @@ package com.example.demo.Controller;
 
 import java.net.URI;
 import java.net.URL;
+import java.util.Base64;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +24,9 @@ public class StatController {
 
     private static final String CLIENT_ID = "7ea1798ef6a540319a8413ce0c0c012e";
     private static final String REDIRECT_URI = "http://localhost:8080/spotify/callback";
-
+    private static final String CLIENT_SECRET = "abaf6f915a1143018ed0009b4258e23e";
+    
+    
     @GetMapping("/login")
     public ResponseEntity<Void> login(HttpServletResponse httpServletResponse) {
         String state = generateRandomString(16);
@@ -35,10 +42,28 @@ public class StatController {
        
     }
       
+    
     @GetMapping("/callback")
-    public String callback(@RequestParam(value = "code") String code) {
-    	return code;
+    public ResponseEntity<String> call(@RequestParam(value = "code") String code, @RequestParam(value = "state") String state){
+      
+    	MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("code", code);
+        requestBody.add("redirect_uri", REDIRECT_URI);
+        requestBody.add("grant_type", "authorization_code");
+        
+        String credentials = CLIENT_ID + ":" + CLIENT_SECRET;
+        String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Basic " + encodedCredentials);
+        
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(
+                "https://accounts.spotify.com/api/token", requestEntity, String.class);
+        return responseEntity;
     }
+
     
     private String generateRandomString(int length) {
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
